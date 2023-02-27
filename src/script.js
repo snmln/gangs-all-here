57a03ee34426468f66973faa302e583c9d128b9f
Sean.Malone <sean.malone@insight.com>
2023-02-26 19:10:40 -0500

    initial shape setup

--- src/script.js
+++ src/script.js
@@ -1,64 +1,36 @@
 import './style.css'
 import * as THREE from 'three'
 import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js'
-import * as dat from 'dat.gui'
-
-// Debug
-const gui = new dat.GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
-
 // Scene
 const scene = new THREE.Scene()
 
 // Objects
 const geometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
+const tourous = new THREE.Mesh(geometry)
 
-// Materials
-
-const material = new THREE.MeshBasicMaterial()
-material.color = new THREE.Color(0xff0000)
+    let floorGeometry = new THREE.BoxGeometry(50, 1, 50);
+    let material = new THREE.MeshBasicMaterial({color: 0xff0000});
+    const floor = new THREE.Mesh( floorGeometry );
+    floor.position.set(0, -1, 0);
 
 // Mesh
-const sphere = new THREE.Mesh(geometry,material)
-scene.add(sphere)
+scene.add(tourous)
+scene.add(floor);
 
-// Lights
-
-const pointLight = new THREE.PointLight(0xffffff, 0.1)
-pointLight.position.x = 2
-pointLight.position.y = 3
-pointLight.position.z = 4
-scene.add(pointLight)
-
-/**
- * Sizes
- */
 const sizes = {
+    //This is not dynamic we will have to establish an event listener to listen for 
     width: window.innerWidth,
     height: window.innerHeight
 }
-
-window.addEventListener('resize', () =>
-{
-    // Update sizes
-    sizes.width = window.innerWidth
-    sizes.height = window.innerHeight
-
-    // Update camera
-    camera.aspect = sizes.width / sizes.height
-    camera.updateProjectionMatrix()
-
-    // Update renderer
-    renderer.setSize(sizes.width, sizes.height)
-    renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-})
 
 /**
  * Camera
  */
 // Base camera
+//this camera we are establishing is what ALL our view will go through. 
 const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
 camera.position.x = 0
 camera.position.y = 0
@@ -72,34 +44,12 @@ scene.add(camera)
 /**
  * Renderer
  */
+//We need to instantiate an instance of WEBGL to render all of our elements. 
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas
+    canvas: canvas,
+   
 })
+console.log('sizes.width', sizes.width, 'sizes.height', sizes.height)
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-
-/**
- * Animate
- */
-
-const clock = new THREE.Clock()
-
-const tick = () =>
-{
-
-    const elapsedTime = clock.getElapsedTime()
-
-    // Update objects
-    sphere.rotation.y = .5 * elapsedTime
-
-    // Update Orbital Controls
-    // controls.update()
-
-    // Render
-    renderer.render(scene, camera)
-
-    // Call tick again on the next frame
-    window.requestAnimationFrame(tick)
-}
-
-tick()
+renderer.render(scene, camera)
