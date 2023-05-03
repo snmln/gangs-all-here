5107c8ad35dbb9f1f65f942913130b1401d9d6df
Sean.Malone <sean.malone@insight.com>
2023-05-03 12:39:44 -0400

    cleaning up files

--- src/script.js
+++ src/script.js
@@ -1,51 +1,56 @@
-import './style.css'
-import * as THREE from 'three'
+import "./style.css";
+import * as THREE from "three";
 
-// Canvas
-const canvas = document.querySelector('canvas.webgl')
-// Scene
-const scene = new THREE.Scene()
+// Establishing Canvas
+const canvas = document.querySelector("canvas.webgl");
+// Establishing Scene
+const scene = new THREE.Scene();
 
-// Objects
-const torousGeometry = new THREE.TorusGeometry( .7, .2, 16, 100 );
-let torousMaterial = new THREE.MeshBasicMaterial({color: 0xeeeee4});
-const tourous = new THREE.Mesh(torousGeometry, torousMaterial)
+// Establishing Objects
+const torousGeometry = new THREE.TorusGeometry(0.7, 0.2, 16, 100);
+let torousMaterial = new THREE.MeshBasicMaterial({ color: 0xeeeee4 });
+const tourous = new THREE.Mesh(torousGeometry, torousMaterial);
 
-    let floorGeometry = new THREE.BoxGeometry(50, 1, 50);
-    let material = new THREE.MeshBasicMaterial({color: 0xff0000});
-    const floor = new THREE.Mesh( floorGeometry, material );
-    floor.position.set(0, -1.5, 0);
+let floorGeometry = new THREE.BoxGeometry(50, 1, 50);
+let material = new THREE.MeshBasicMaterial({ color: 0xff0000 });
+const floor = new THREE.Mesh(floorGeometry, material);
+floor.position.set(0, -1.5, 0);
 
-// Mesh
-scene.add(tourous)
+// Addng objects to scene
+scene.add(tourous);
 scene.add(floor);
 
 const sizes = {
-    //This is not dynamic we will have to establish an event listener to listen for 
-    width: window.innerWidth,
-    height: window.innerHeight
-}
+  //This is not dynamic we will have to establish an event listener to listen for
+  width: window.innerWidth,
+  height: window.innerHeight,
+};
 
 /**
  * Camera
  */
 // Base camera
-//this camera we are establishing is what ALL our view will go through. 
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.x = 0
-camera.position.y = 0
-camera.position.z = 2
-scene.add(camera)
+//this camera we are establishing is what ALL our view will go through.
+const camera = new THREE.PerspectiveCamera(
+  75,
+  sizes.width / sizes.height,
+  0.1,
+  100
+);
+camera.position.x = 0;
+camera.position.y = 0;
+camera.position.z = 2;
 
+// Addng Camera to scene
+scene.add(camera);
 
 /**
  * Renderer
  */
-//We need to instantiate an instance of WEBGL to render all of our elements. 
+//We need to instantiate an instance of WEBGL to render all of our elements through it.
 const renderer = new THREE.WebGLRenderer({
-    canvas: canvas,
-   
-})
-renderer.setSize(sizes.width, sizes.height)
-renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
-renderer.render(scene, camera)
+  canvas: canvas,
+});
+renderer.setSize(sizes.width, sizes.height);
+renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+renderer.render(scene, camera);
