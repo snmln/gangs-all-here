57c400d921b2424e4a2bc5b3ff7ec2728b2cf69e
Sean.Malone <sean.malone@insight.com>
2023-05-15 11:23:53 -0400

    updating tube geometry color.

--- src/script.js
+++ src/script.js
@@ -78,7 +78,7 @@ let insightCorp = {
 const tubeFragment = `
 varying vec2 vertexUV; 
 void main(){
-    gl_FragColor = vec4(vertexUV.x,0.,0.0,1.);
+    gl_FragColor = vec4(1.0,0.0,1.0,1.0);
 }`;
 
 const tubeVertex = `
